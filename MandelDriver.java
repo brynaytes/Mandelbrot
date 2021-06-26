2c5e930e695271d3095140d2816b298447926cab
brynaytes <45157398+brynaytes@users.noreply.github.com>
2021-06-26 12:10:57 -0500

    Updating to newest version

--- MandelDriver.java
+++ MandelDriver.java
@@ -1,4 +1,3 @@
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -6,6 +5,7 @@ import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
@@ -14,26 +14,27 @@ import javax.swing.SwingUtilities;
 import java.awt.event.*;
 
 public class MandelDriver extends JFrame{
-	public static int width = 600;
-	public static int height = 400;
-	public static int x=0,y=0;
-	public static int imageOffsetX = width;
-	public static int imageOffsetY = height;
-	public static int zoom = 300;
-	public static JLabel L;
-	public static JFrame f;
+	public  int width = 600;
+	public  int height = 400;
+	public  int x=0,y=0;
+	public  int r,g,b, rgb,incrementWhite, zoomval,maxIncrement;
+	public  int imageOffsetX = width;
+	public  int imageOffsetY = height;
+	public  int zoom = 300;
+	public  JLabel L;
+	public  JFrame f;
 	public static void main(String[] args) {
 		new MandelDriver();
 	}
-	public static JLabel zoomLabel = new JLabel("Zoom:");
-	public static JSlider zoomSlider = new JSlider(JSlider.HORIZONTAL,0,1000,100);
-	public static JLabel iterationLabel = new JLabel("Iterations:");
-	public static JTextField iterationField = new JTextField("50",4);
+	public  JLabel zoomLabel = new JLabel("Zoom:");
+	public  JSlider zoomSlider = new JSlider(JSlider.HORIZONTAL,0,1000,100);
+	public  JLabel iterationLabel = new JLabel("Iterations:");
+	public  JTextField iterationField = new JTextField("50",4);
 
-	public static JLabel screenWLabel = new JLabel("W:");
-	public static JTextField screenW = new JTextField(width+"",4);	
-	public static JLabel screenHLabel = new JLabel("H:");
-	public static JTextField screenH = new JTextField(height+"",4);	
+	public  JLabel screenWLabel = new JLabel("W:");
+	public  JTextField screenW = new JTextField(width+"",4);	
+	public  JLabel screenHLabel = new JLabel("H:");
+	public  JTextField screenH = new JTextField(height+"",4);	
 	
 	//Create frame and listen for clicks
 	public MandelDriver() {
@@ -64,6 +65,7 @@ public class MandelDriver extends JFrame{
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
+				try {
 				if((f.getWidth() != Integer.parseInt(screenW.getText()) || f.getHeight() != Integer.parseInt(screenH.getText()) )&& Integer.parseInt(screenW.getText()) !=0 && Integer.parseInt(screenH.getText() )!=0){
 					width = Integer.parseInt(screenW.getText());
 					height = Integer.parseInt(screenH.getText());
@@ -75,30 +77,39 @@ public class MandelDriver extends JFrame{
 				x = e.getX();
 				y = e.getY();
 				if(x > width/2 && y<height/2) {
-					imageOffsetX = imageOffsetX - 100;
-					imageOffsetY = imageOffsetY + 100;
+					imageOffsetX = imageOffsetX - Math.abs(width/2 - e.getX());
+					imageOffsetY = imageOffsetY +  Math.abs(height/2 - e.getY());
 				}else if(x < width/2 && y<height/2) {
-					imageOffsetX = imageOffsetX + 100;
-					imageOffsetY = imageOffsetY + 100;
+					imageOffsetX = imageOffsetX + Math.abs(width/2 - e.getX());
+					imageOffsetY = imageOffsetY +  Math.abs(height/2 - e.getY());
 				}else if(x > width/2 && y>height/2) {
-					imageOffsetX = imageOffsetX - 100;
-					imageOffsetY = imageOffsetY - 100;
+					imageOffsetX = imageOffsetX - Math.abs(width/2 - e.getX());
+					imageOffsetY = imageOffsetY -  Math.abs(height/2 - e.getY());
 				}else if(x < width/2 && y>height/2) {
-					imageOffsetX = imageOffsetX + 100;
-					imageOffsetY = imageOffsetY - 100;
+					imageOffsetX = imageOffsetX + Math.abs(width/2 - e.getX());
+					imageOffsetY = imageOffsetY -   Math.abs(height/2 - e.getY());
 				}
 				//	imageOffsetX =  (imageOffsetX + x) /2;
 				//	imageOffsetY = (imageOffsetY + y)/2;
 				//zoom = zoom + 100;
 				}else if(SwingUtilities.isMiddleMouseButton(e))
 				{
+					System.out.println("middle click");
 					zoom = zoom -zoomSlider.getValue();
+					
 				}else {
-					zoom = zoom + zoomSlider.getValue();
+					zoomval =  zoomSlider.getValue();
+					zoom = zoom + zoomval;
+					if(zoomval < 100) zoomval = 100;
+					imageOffsetX += 120* (zoomval/100);
+					imageOffsetY -= 45* (zoomval/100) ;
 				}
 				
+				
 				L.setIcon( new ImageIcon(CreateImage()));
-
+				}catch( NumberFormatException numEx) {
+					JOptionPane.showMessageDialog(f,"The input fields can only contain numbers. Remove text or spaces.", "Error", JOptionPane.PLAIN_MESSAGE);
+				}
 			}
 		});
 	}
@@ -106,28 +117,45 @@ public class MandelDriver extends JFrame{
 	
 	
 	//returns mandelbrot set image
-	public static BufferedImage CreateImage() {
+	public  BufferedImage CreateImage() {
 		//System.out.println("making image");
 		BufferedImage img = new BufferedImage(width,height,BufferedImage.TYPE_INT_RGB);
 		int temp=0;
+		maxIncrement = Integer.parseInt(iterationField.getText());
+		incrementWhite = 5; //was 4
+	//	if(maxIncrement > 100)incrementWhite = 3;else if(maxIncrement > 200)incrementWhite = 1;
 		
+		if(incrementWhite < 1) {incrementWhite = 1;}
 		//Loop to create the actual set
+	//	threaded t = new threaded();
 		for(int y =0; y< height;y++) {
 			for(int x=0;x < width; x++) {
-				//y is originally 200
-				temp = IsStable((double)(x-(imageOffsetX)) /zoom,(double)(y-imageOffsetY/2)/zoom,Integer.parseInt(iterationField.getText())) ;
-				//temp = IsInside.IsStable((double)x /zoom,((double)y-200)/zoom)  * 5;
+				temp = IsStable((double)(x-(imageOffsetX+100)) /zoom,(double)(y-imageOffsetY/2)/zoom,maxIncrement) ;
+				temp *= incrementWhite;
+		
+				if(temp >  255) {
+					temp = 255;
+				}
+					
+		 	/*	if(temp < (Integer.parseInt(iterationField.getText()))){
+				v = 255;
+				}else {
+					v=0;
+				}
 				
-				if(temp > 255) temp=255;
-				if(temp<0) temp =0;
-			//	System.out.println(temp);
+				rgb = Color.HSBtoRGB(255 * temp / Integer.parseInt(iterationField.getText()), 255, v);
+				r = (rgb >> 16) & 0xFF;
+				g = (rgb >> 8) & 0xFF;
+				b = (rgb & 0xFF);
+			*/
 				img.setRGB(x,y,new Color(temp,temp,temp).getRGB());
+				
 			}
 		}
 		return img;
 	}
 	
-	public static int IsStable(double x, double y,int iterations) {
+	public  int IsStable(double x, double y,int iterations) {
 		double constantx =x;
 		double constanty= y;
 		double xz=0;
@@ -137,10 +165,11 @@ public class MandelDriver extends JFrame{
 			yz = 2 * x *y;
 			x = xz + constantx;
 			y=yz+constanty;
-			if(Math.abs(y) > 2 || Math.abs(x) > 2 ) {return (int)((double)(iterations -i)*(255/(double)iterations));}
+			if(Math.abs(y) > 2 || Math.abs(x) > 2)return iterations - i;
+			//if(Math.abs(y) > 2 || Math.abs(x) > 2 ) {return (int)((double)(iterations -i)*(255/(double)iterations));}
 		}	
 		if(	Math.abs(y) > 1 || Math.abs(x) > 1 )
 			return 1;
 		return 0;
-	}
+	}	
 }
